cf1b76b7572f59b6c7c4ec7cddb581db21bdb1b3
Zoey Yancy <yancyshmancy@gmail.com>
2016-03-08 12:16:21 -0500

    Added D3 chart

---
+++ chart.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = (function() {
+    class comicBookCompany {
+
+        constructor(name) {
+            this.name = name;
+            this.x = 2;
+        }
+    }
+
+    return {
+        comicBookCompany
+    }
+})();
---
+++ index.js
@@ -0,0 +1,67 @@
+var hide = function() {
+    $(this).addClass("hide");
+};
+
+var dataset = [ 40, 26, 9, 5, 3, 2 ];
+
+var w = $('#myChart').width();
+var h = $('#myChart').height();
+var barPadding = 10;
+
+var svg = d3.select("#myChart")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("id", "chart")
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {
+        return i * (w / dataset.length);
+    })
+    .attr("y", h)
+    .attr("width", w/dataset.length - barPadding)
+    .style("fill", function(d) {
+        return "rgb(0, 0, " + (d * 10) + ")";
+    })
+    .attr("height", 0 )
+    .transition()
+        .attr("height",  function(d){
+            return d * 10;
+        })
+        .attr("y", function(d) {
+            return h - d * 10; // height minus data value
+        })
+        .duration(2000)
+
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return d;
+    })
+    .attr("x", function(d, i) {
+        return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+    })
+    .attr("y", function(d) {
+        return h - (d * 10) + 20;
+    })
+    .attr("font-family", "helvetica")
+    .attr("font-size", "11px")
+    .attr("fill", "white")
+    .attr("text-anchor", "middle")
+    .attr("opacity", 0)
+    .transition()
+        .attr("opacity", 1)
+        .duration(2000)
+
+
+
+
+
+
+
